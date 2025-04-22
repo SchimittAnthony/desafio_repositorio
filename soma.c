e5a926c752571296552c08730399aee4d828d6eb
SchimittAnthony <anthony.schimitt@gmail.com>
2025-04-22 20:25:14 -0300

    Update soma.c

--- soma.c
+++ soma.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, soma;
-  
-    printf("Digite o primeiro número: ");
-    scanf("%d", &num1);
-
-    printf("Digite o segundo número: ");
-    scanf("%d", &num2);
+    int num1 = 7;
+    int num2 = 5;
   
     soma = num1 + num2;
   
